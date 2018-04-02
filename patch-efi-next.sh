e77dac10bea5fbacd63f6e17d32f6a7ef23b6231
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-04-02 19:39:40 +0200

    0001-ARM-vexpress-fdtfile-fdt_addr_r-for-vexpress_ca15_tc.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -37,6 +37,7 @@ git am ../patch/0001-usb-ehci-faraday-remove-unused-driver.patch
 git am ../patch/0001-usb-ehci-hcd-eliminate-tweaks-used-for-faraday.patch
 git am ../patch/0001-lib-slre-remove-superfluous-assignment.patch
 git am ../patch/0001-log-fix-typo-LOGL_EFI.patch
+git am ../patch/0001-ARM-vexpress-fdtfile-fdt_addr_r-for-vexpress_ca15_tc.patch
 
 # Video console
 git am ../patch/0001-video-if-EFI_LOADER-is-enabled-enable-CFB_CONSOLE_AN.patch
