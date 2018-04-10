afb8c18dafe24a1ef60b4a82c736eb4a49805363
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-04-11 01:05:07 +0200

    0001-efi_loader-no-support-for-ARMV7_NONSEC-y.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -62,7 +62,9 @@ git am ../patch/0001-efi_loader-allow-unaligned-memory-access.patch
 git am ../patch/0001-arm-armv7-allow-unaligned-memory-access.patch
 git am ../patch/0001-efi_selftest-test-unaligned-memory-access.patch
 
+# 32-bit support
 # git am ../patch/0001-efi_loader-support-for-CONFIG_ARMV7_NONSEC.patch
+git am 0001-efi_loader-no-support-for-ARMV7_NONSEC-y.patch
 
 exit
 # From Simon's Sandbox support series
