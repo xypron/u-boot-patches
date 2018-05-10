6eaf6f811165eaaa3f9ec2bac90a0fe5e0bfaf5e
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-05-10 09:37:38 +0200

    0001-efi_loader-implement-ReinstallProtocolInterface.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -48,6 +48,7 @@ git am ../patch/0001-usb-kbd-implement-special-keys.patch
 # diverse protocols
 git am ../patch/0001-efi_loader-efi_allocate_pages-is-too-restrictive.patch
 git am ../patch/0001-efi_loader-avoid-using-unitialized-values-as-console.patch
+git am ../patch/0001-efi_loader-implement-ReinstallProtocolInterface.patch
 
 # Unaligned access
 git am ../patch/0001-cmd-add-exception-command.patch
