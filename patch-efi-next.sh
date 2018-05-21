83cd4a8338a78188399a7e3f198056cb9ad5d138
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-05-21 12:26:05 +0200

    0001-logos-provide-U-Boot-logo.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -34,6 +34,7 @@ git am ../patch/0001-ARM-vexpress-fdtfile-fdt_addr_r-for-vexpress_ca15_tc.patch
 git am ../patch/0001-doc-expand-README.commands.patch
 git am ../patch/0001-Makefile-KCFLAGS-std-gnu11.patch
 git am ../patch/0001-Makefile-clean-should-delete-.efi.patch
+git am ../patch/0001-logos-provide-U-Boot-logo.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
