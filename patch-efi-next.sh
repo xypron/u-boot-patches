774436d8f2edc53d04b4e279e12a67ee4ce508e5
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-06-27 20:32:47 +0200

    0001-efi_loader-allocate-configuration-table-array.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -47,7 +47,11 @@ git am ../patch/0001-efi_selftest-test-writing-to-file.patch
 git am ../patch/0001-cmd-add-exception-command.patch
 
 # Configuration tables
-git am ../patch/0001-efi_loader-increase-number-of-configuration-tables.patch
+git am ../patch/0001-efi_loader-Increase-number-of-configuration-tables-t.patch
+git am ../patch/0001-efi_loader-specify-UEFI-spec-revision.patch
+git am ../patch/0001-efi_loader-correct-headersize-of-runtime-services-ta.patch
+git am ../patch/0001-efi_loader-calculate-crc32-for-EFI-tables.patch
+git am ../patch/0001-efi_loader-allocate-configuration-table-array.patch
 
 # Firefly-RK3399
 git am ../patch/0001-rockchip-evb-rk3399-correct-README-for-board-bring-u.patch
