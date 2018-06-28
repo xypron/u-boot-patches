7bf08c7c12ab2ea5e9788548a3de78ff7953d80f
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-06-29 00:51:00 +0200

    0001-ARM-qemu-arm-enable-RTC.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -33,6 +33,7 @@ git am ../patch/0001-rockchip-doc-clarify-usage-of-CONFIG_SPL_ROCKCHIP_BA.patch
 git am ../patch/0001-common-print-n-in-initr_scsi.patch
 git am ../patch/0001-Sandbox-provide-default-dtb.patch
 git am ../patch/0001-lib-div64-fix-typeo-in-include-div64.h.patch
+git am ../patch/0001-ARM-qemu-arm-enable-RTC.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
