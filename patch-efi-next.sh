dbf40a6f2d528ea4b2271843f2ec8d6b10a7191c
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-07-12 19:41:50 +0200

    0001-lib-charset-upper-lower-case-conversion.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -66,6 +66,7 @@ git am ../patch/0001-lib-build-charset.o-only-if-needed.patch
 git am ../patch/0001-efi_loader-rename-utf16_strlen-utf16_strnlen.patch
 git am ../patch/0001-lib-charset-utility-functions-for-utf-16.patch
 git am ../patch/0001-efi_loader-capitalization-table.patch
+git am ../patch/0001-lib-charset-upper-lower-case-conversion.patch
 git am ../patch/0001-test-unit-tests-for-Unicode-functions.patch
 git am ../patch/0001-efi_loader-EFI_UNICODE_COLLATION_PROTOCOL.patch
 # git am ../patch/0001-lib-refactor-utf16_to_utf8.patch
