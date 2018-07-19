404171f1a8d0f7f234f8cd3a6fd8cb92f08e356e
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-07-19 07:34:42 +0200

    0001-lib-vsprintf-correct-printing-of-Unicode-strings.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -65,6 +65,7 @@ git am ../patch/0001-lib-build-charset.o-only-if-needed.patch
 git am ../patch/0001-efi_loader-rename-utf16_strlen-utf16_strnlen.patch
 git am ../patch/0001-lib-charset-utility-functions-for-Unicode.patch
 git am ../patch/0001-test-unit-tests-for-Unicode-functions.patch
+git am ../patch/0001-lib-vsprintf-correct-printing-of-Unicode-strings.patch
 git am ../patch/0001-efi_loader-capitalization-table.patch
 git am ../patch/0001-lib-charset-upper-lower-case-conversion.patch
 git am ../patch/0001-test-tests-for-utf_to_lower-utf_to_upper.patch
