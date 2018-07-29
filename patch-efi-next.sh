dc31e913bf21fb4c9fa81229d9c2b94fc06208c6
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-07-29 10:41:48 +0200

    0001-drivers-serial-document-on_baudrate.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -32,6 +32,7 @@ git am ../patch/0001-lib-slre-remove-superfluous-assignment.patch
 git am ../patch/0001-ARM-vexpress-fdtfile-fdt_addr_r-for-vexpress_ca15_tc.patch
 git am ../patch/0001-drivers-rtc-correctly-set-week-day-for-mc146818.patch
 git am ../patch/0001-dm-sysreset-x86-missing-build-dependency.patch
+git am ../patch/0001-drivers-serial-document-on_baudrate.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
