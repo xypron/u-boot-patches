e3d7fd59e64cf89fd24e9f0e6408931b787f8588
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-07-31 06:08:33 +0200

    Remove accepted patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -30,7 +30,6 @@ git am ../patch/0001-usb-xhci-remove-superfluous-assignment.patch
 git am ../patch/0001-usb-musb-remove-superfluous-assignment.patch
 git am ../patch/0001-lib-slre-remove-superfluous-assignment.patch
 git am ../patch/0001-ARM-vexpress-fdtfile-fdt_addr_r-for-vexpress_ca15_tc.patch
-git am ../patch/0001-drivers-rtc-correctly-set-week-day-for-mc146818.patch
 git am ../patch/0001-dm-sysreset-x86-missing-build-dependency.patch
 git am ../patch/0001-drivers-serial-document-on_baudrate.patch
 git am ../patch/0001-README-U_BOOT_ENV_CALLBACK-functions.patch
