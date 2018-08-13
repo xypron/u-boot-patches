3faa2d03d6241e2eab8e9eabda7c32d2545ec490
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-08-13 19:20:25 +0200

    0001-efi_loader-memory-device-path-for-booting.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -74,6 +74,9 @@ git am ../patch/0001-test-tests-for-utf_to_lower-utf_to_upper.patch
 git am ../patch/0001-efi_loader-EFI_UNICODE_COLLATION_PROTOCOL.patch
 git am ../patch/0001-efi_selftest-EFI_UNICODE_COLLATION_PROTOCOL.patch
 
+# Deletion of handles
+git am ../patch/0001-efi_loader-memory-device-path-for-booting.patch
+
 # KGDB
 # git am ../patch/0001-arm-register-numbers-for-GDB.patch
 # git am ../patch/0001-arm-create-templates-for-kgdb.patch
