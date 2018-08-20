b3aec66baf93c4354f157588a77cc5b5e8f46429
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-08-20 19:31:50 +0200

    0001-efi_loader-refactor-loaded-image-handle.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -76,6 +76,7 @@ git am ../patch/0001-efi_selftest-EFI_UNICODE_COLLATION_PROTOCOL.patch
 git am ../patch/0001-efi_loader-do-not-use-local-variable-for-handle.patch
 git am ../patch/0001-efi_loader-memory-leak-in-efi_set_bootdev.patch
 git am ../patch/0001-efi_loader-refactor-efi_setup_loaded_image.patch
+git am ../patch/0001-efi_loader-refactor-loaded-image-handle.patch
 
 # KGDB
 # git am ../patch/0001-arm-register-numbers-for-GDB.patch
