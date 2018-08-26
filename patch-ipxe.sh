ffb1b74491048b644773dfbe99ff3170b02a0cbb
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-08-26 12:00:52 +0200

    0001-build-Fix-building-with-gcc-8.2.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-ipxe.sh
+++ patch-ipxe.sh
@@ -3,3 +3,4 @@ git am --abort || true
 
 git am ../patch/0001-build-Don-t-use-mabi-lp64-for-HOSTCC.patch
 git am ../patch/0001-efi-avoid-stringop-truncation-error-in-util-elf2efi..patch
+git am ../patch/0001-build-Fix-building-with-gcc-8.2.patch
