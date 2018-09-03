7335b949c7b3d5c83e50841d7426a262a037070a
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-03 19:13:21 +0200

    0001-efi_loader-fix-typos-in-efi_boottime.c.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -38,6 +38,7 @@ git am ../patch/0001-arm-qemu-arm-enable-PL031-RTC-in-defconfig.patch
 # Unicode collation protocol
 # git am ../patch/0001-efi_loader-provide-description-of-file_open.patch
 # git am ../patch/0001-efi_loader-check-parameters-of-efi_file_open.patch
+git am ../patch/0001-efi_loader-fix-typos-in-efi_boottime.c.patch
 git am ../patch/0001-efi_loader-capitalization-table.patch
 git am ../patch/0001-lib-charset-upper-lower-case-conversion.patch
 git am ../patch/0001-test-tests-for-utf_to_lower-utf_to_upper.patch
