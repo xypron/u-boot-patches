7db0108a6f43350f47877442bc641d3c1144d899
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-15 23:54:41 +0200

    Remove accepted patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -42,8 +42,6 @@ git am ../patch/0001-arm-qemu-arm-enable-PL031-RTC-in-defconfig.patch
 # Workarounds for running EFI shell
 git am ../patch/0001-efi_loader-add-dummy-efi-tables-for-booting-Shell.ef.patch
 git am ../patch/0001-efi_loader-Initial-HII-protocols.patch
-git am ../patch/0001-efi_loader-environment-variables-for-terminal-size.patch
-git am ../patch/0001-efi_selftest-missing-braces-in-efi_selftest_textinpu.patch
 git am ../patch/0001-efi_loader-query-serial-console-size-reliably.patch
 exit
 
