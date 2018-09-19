1d47ac85df79543eb677683329ea9c6e92ac07f7
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-09-19 18:25:21 +0200

    0001-test-py-catch-errors-occurring-when-reading-the-conso.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -23,7 +23,7 @@ git am ../patch/0001-docs-kernel-doc-fix-parsing-of-function-pointers.patch
 git am ../patch/0001-x86-qemu-x86-requires-CONFIG_SYS_HZ-2000.patch
 git am ../patch/0001-cmd-add-conitrace-command.patch
 git am ../patch/0001-sandbox-Build-with-fPIC.patch
-git am ../patch/0001-test-py-catch-errors-occuring-when-reading-the-conso.patch
+git am ../patch/0001-test-py-catch-errors-occurring-when-reading-the-cons.patch
 git am ../patch/0001-efi_driver-convert-function-descriptions-to-Sphinx-s.patch
 git am ../patch/0001-dm-video-check-bounds-for-column-and-row.patch
 
