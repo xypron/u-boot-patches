36a80acca9c19383d7203c944abefb9d677ff29b
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-10-03 21:15:17 +0200

    0001-efi_loader-EFI_ENTRY-print-device-path.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -33,6 +33,7 @@ git am ../patch/0001-efi_loader-return-type-efi_console_register.patch
 git am ../patch/0001-efi_loader-error-handling-in-read_console.patch
 git am ../patch/0001-fs-fat-memory-leak-in-fat_unlink.patch
 git am ../patch/0001-efi_loader-efi_allocate_pool-EFI_ALLOCATE_ANY_PAGES.patch
+git am ../patch/0001-efi_loader-EFI_ENTRY-print-device-path.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
