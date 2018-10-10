5dc877c08fd87020c6fb17142e27a7f3291e45bf
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-10-11 01:33:45 +0200

    0001-efi_loader-PSCI-reset-and-shutdown-for-EL1.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -32,6 +32,7 @@ git am ../patch/0001-efi_loader-efi_allocate_pool-EFI_ALLOCATE_ANY_PAGES.patch
 git am ../patch/0001-efi_loader-execute-efi_save_gd-first.patch
 git am ../patch/0001-efi_loader-correct-signature-of-GetPosition-SetPosit.patch
 git am ../patch/0001-doc-README.iscsi-Open-iSCSI-configuration.patch
+git am ../patch/0001-efi_loader-PSCI-reset-and-shutdown-for-EL1.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
