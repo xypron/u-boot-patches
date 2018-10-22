94f6839a374af33b39ca4895834cea0614791c96
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-10-22 23:08:01 +0200

    Remove accepted patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -21,7 +21,6 @@ git am ../patch/0001-test-py-catch-errors-occurring-when-reading-the-cons.patch
 git am ../patch/0001-doc-README.iscsi-Open-iSCSI-configuration.patch
 git am ../patch/0001-cmd-add-el-command.patch
 git am ../patch/0001-cmd-bdinfo-correct-output-of-numerical-values.patch
-git am ../patch/0001-x86-put-global-data-pointer-into-the-.data-section.patch
 git am ../patch/0001-sandbox-remove-stray-DEBUG.patch
 git am ../patch/0001-sandbox-README-setting-environment-variables.patch
 git am ../patch/0001-sandbox-README-use-setenv-ethrotate-no.patch
