a4a468473d3a2185861f2b4e9ab80902ed692c83
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-11-10 11:38:09 +0100

    0001-efi_selftest-check-fdt-is-marked-as-runtime-data.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -54,6 +54,7 @@ git am ../patch/0001-efi_selftest-add-test-for-memory-allocation.patch
 git am ../patch/0001-efi_selftest-building-sandbox-with-EFI_SELFTEST.patch
 
 git am ../patch/0001-efi_loader-memory-reservation-for-fdt.patch
+git am ../patch/0001-efi_selftest-check-fdt-is-marked-as-runtime-data.patch
 exit
 
 git am ../patch/0001-efi_loader-set-image_base-and-image_size-to-correct-.patch
