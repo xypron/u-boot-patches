1ebb24efe29ec85b973f0f5ab869f1806f052a92
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-11-10 19:45:30 +0100

    0001-board-toradex-colibri_vf-efi_loader-unset-CONFIG_EFI.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -27,7 +27,7 @@ git am ../patch/0001-sandbox-README-use-setenv-ethrotate-no.patch
 git am ../patch/0001-dm-video-correctly-set-the-cursor-position.patch
 git am ../patch/0001-fs-fat-validate-sector-and-cluster-size.patch
 git am ../patch/0001-efi_loader-use-u16-for-UTF16-strings.patch
-git am ../patch/0001-configs-disable-Unicode-capitalization-on-VF610.patch
+git am ../patch/0001-board-toradex-colibri_vf-efi_loader-unset-CONFIG_EFI.patch 
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
