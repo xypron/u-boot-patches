871a0f56c3ba97945c881857cc816a8d8630f011
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-11-17 07:29:25 +0100

    0001-efi_loader-fix-memory-mapping-for-sandbox.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -53,9 +53,9 @@ git am ../patch/0001-efi_loader-fix-simple-network-protocol.patch
 git am ../patch/0001-efi_loader-eliminate-sandbox-addresses.patch
 git am ../patch/0001-efi_selftest-add-test-for-memory-allocation.patch
 git am ../patch/0001-efi_selftest-building-sandbox-with-EFI_SELFTEST.patch
+git am ../patch/0001-efi_loader-macro-efi_size_in_pages.patch
 
 git am ../patch/0001-fdt_support-fdt-reservations-on-the-sandbox.patch
-git am ../patch/0001-efi_loader-macro-efi_size_in_pages.patch
 git am ../patch/0001-efi_loader-fix-memory-mapping-for-sandbox.patch
 git am ../patch/0001-efi_loader-do-not-use-magic-address-for-fdt.patch
 git am ../patch/0001-efi_selftest-check-fdt-is-marked-as-runtime-data.patch
