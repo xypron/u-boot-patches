fbcb5aae921923fe810d199b57b3568e1aafe96c
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-11-25 09:23:16 +0100

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -20,9 +20,6 @@ git am ../patch/0001-x86-qemu-x86-requires-CONFIG_SYS_HZ-2000.patch
 git am ../patch/0001-test-py-catch-errors-occurring-when-reading-the-cons.patch
 git am ../patch/0001-doc-README.iscsi-Open-iSCSI-configuration.patch
 # git am ../patch/0001-cmd-add-el-command.patch
-git am ../patch/0001-sandbox-remove-stray-DEBUG.patch
-git am ../patch/0001-sandbox-README-setting-environment-variables.patch
-git am ../patch/0001-sandbox-README-use-setenv-ethrotate-no.patch
 git am ../patch/0001-dm-video-correctly-set-the-cursor-position.patch
 git am ../patch/0001-fs-fat-validate-sector-and-cluster-size.patch
 git am ../patch/0001-MAINTAINERS-add-EFI-PAYLOAD-reviewer.patch
