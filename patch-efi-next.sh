3667b55ed71336df0d7acc8265547f7405fb07da
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-12-10 07:19:15 +0100

    0001-efi_loader-Make-RTS-relocation-more-robust.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -21,8 +21,8 @@ git am ../patch/0001-fs-fat-validate-sector-and-cluster-size.patch
 # git am ../patch/0001-test-py-replace-deprecated-item.get_marker.patch
 git am ../patch/0001-test-overlay-NULL-passed-as-fdt.patch
 # git am ../patch/0001-efi_loader-revert-Align-runtime-section-to-64kb.patch
-git am ../patch/0001-efi_loader-Odroid-C2-does-not-boot-with-U-Boot-maste.patch
 git am ../patch/0001-efi_loader-efi_connect_controller-use-pD.patch
+git am ../patch/0001-efi_loader-Make-RTS-relocation-more-robust.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
