99a07f7126ed0ceb3cb1cbbb366f319bba59a04f
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-12-18 22:32:37 +0100

    0001-efi_loader-signature-of-efi_set_virtual_address_map.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -27,6 +27,7 @@ git am ../patch/0001-tests-enable-unit-tests-by-default-on-QEMU-ARM-64.patch
 git am ../patch/0001-tools-imx8image-use-correct-printf-escape-sequence.patch
 git am ../patch/0001-efi_loader-struct-efi_configuration_table.patch
 git am ../patch/0001-lib-crc32-mark-static-variable-as-__efi_runtime_data.patch
+git am ../patch/0001-efi_loader-signature-of-efi_set_virtual_address_map.patch
 git am ../patch/0001-rockchip-sysreset-enable-UEFI-reboot.patch
 
 # USB keyboard
