980207eada6a27509d4a730b7832ef5ba6d30d9c
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-12-20 21:26:03 +0100

    0001-arm64-mvebu-armada-8k-support-booting-from-NVME.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -31,6 +31,7 @@ git am ../patch/0001-efi_loader-signature-of-efi_set_virtual_address_map.patch
 git am ../patch/0001-rockchip-sysreset-enable-UEFI-reboot.patch
 git am ../patch/0001-arm64-mvebu-defconfig-enable-CONFIG_CMD_NVME.patch
 git am ../patch/0001-distro_bootcmd-add-NVME-support.patch
+git am ../patch/0001-arm64-mvebu-armada-8k-support-booting-from-NVME.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
