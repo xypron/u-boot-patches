7a1374e073edf0fcf89796b304f91c913df7c8d3
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-12-30 10:47:57 +0100

    0001-efi_selftest-allow-building-on-ARMv7-M.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -48,6 +48,7 @@ git am ../patch/0001-efi_loader-signature-of-StartImage-and-Exit.patch
 git am ../patch/0001-efi_loader-use-efi_start_image-for-bootefi.patch
 
 git am ../patch/0001-efi_loader-CMD_BOOTEFI_HELLO_COMPILE-in-configs.patch
+git am ../patch/0001-efi_selftest-allow-building-on-ARMv7-M.patch
 git am ../patch/0001-efi_loader-consistent-build-flags-for-EFI-applicatio.patch
 git am ../patch/0001-efi_loader-provide-test-files-for-bootmgr.patch
 
