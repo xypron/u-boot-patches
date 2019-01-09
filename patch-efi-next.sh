81ee889e3bf77f79d74fa29f35b7f733c03760a7
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-01-09 21:57:00 +0100

    0001-efi_loader-fix-CopyMem.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -35,6 +35,7 @@ git am ../patch/0001-cmd-tpm-v2-use-correct-format-code.patch
 git am ../patch/0001-cmd-ubi-remove-unreachable-code.patch
 git am ../patch/0001-cmd-unzip-use-correct-format-code.patch
 git am ../patch/0001-cmd-zip-use-correct-format-code.patch
+git am ../patch/0001-efi_loader-fix-CopyMem.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
