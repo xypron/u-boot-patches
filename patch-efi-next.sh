14cece47ef5e5f05b2224d37c5d29d770f0cbf43
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-01-19 18:47:21 +0100

    0001-powerpc-enabled-building-with-CONFIG_DM-y.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -28,6 +28,7 @@ git am ../patch/0001-dm-scsi-report-correct-device-number.patch
 git am ../patch/0001-cmd-usb-display-bus-number.patch
 git am ../patch/0001-efi_loader-remove-duplicate-function-mem2hex.patch
 git am ../patch/0001-efi_loader-simplify-UEFI-variable-storage.patch
+git am ../patch/0001-powerpc-enabled-building-with-CONFIG_DM-y.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
