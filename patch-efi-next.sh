dbd2fd704fc4afd97d4feb591888dfe81039958d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-01-21 18:40:24 +0100

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -26,13 +26,8 @@ git am ../patch/0001-arm64-mvebu-armada-8k-support-booting-from-NVME.patch
 git am ../patch/0001-arm64-dts-marvell-armada-ap806-reserve-PSCI-area.patch
 git am ../patch/0001-dm-scsi-report-correct-device-number.patch
 git am ../patch/0001-cmd-usb-display-bus-number.patch
-git am ../patch/0001-efi_loader-remove-duplicate-function-mem2hex.patch
-git am ../patch/0001-efi_loader-comments-for-variable-services.patch
 git am ../patch/0001-efi_loader-simplify-UEFI-variable-storage.patch
 git am ../patch/0001-powerpc-enabled-building-with-CONFIG_DM-y.patch
-git am ../patch/0001-efi_loader-provide-freestanding-library.patch
-git am ../patch/0001-efi_loader-use-freestanding-library-for-efi-apps.patch
-git am ../patch/0001-efi_loader-use-library-memcpy-in-helloworld.efi.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
