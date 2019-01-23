531cd3d0228f8194b7bdca72e0ba7cc694ef0182
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-01-23 20:40:45 +0100

    0001-efi_selftest-fix-HII-tests.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -28,6 +28,7 @@ git am ../patch/0001-efi_loader-simplify-UEFI-variable-storage.patch
 git am ../patch/0001-powerpc-enabled-building-with-CONFIG_DM-y.patch
 git am ../patch/0001-hashtable-remove-caps-buffer.patch
 git am ../patch/0001-test-provide-unit-test-for-memory-functions.patch
+git am ../patch/0001-efi_selftest-fix-HII-tests.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
