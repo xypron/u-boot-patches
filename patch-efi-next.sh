d6b8759eae3ff4e0f5cc043caaa5803713e0f00f
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-01-28 19:09:35 +0100

    Remove accepted patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -26,7 +26,6 @@ git am ../patch/0001-dm-scsi-report-correct-device-number.patch
 git am ../patch/0001-cmd-usb-display-bus-number.patch
 git am ../patch/0001-efi_loader-simplify-UEFI-variable-storage.patch
 git am ../patch/0001-powerpc-enabled-building-with-CONFIG_DM-y.patch
-git am ../patch/0001-hashtable-remove-caps-buffer.patch
 git am ../patch/0001-test-provide-unit-test-for-memory-functions.patch
 git am ../patch/0001-x86-define-CONFIG_LOADADDR-0x1100000.patch
 git am ../patch/0001-test-py-use-default-load-address-for-tftp.patch
