eb370e56e7e4c0a47f7002015b9c51ff0130c0c4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-02-28 06:06:05 +0100

    0001-efi_loader-do-not-add-in-efi_fs_from_name.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -26,6 +26,7 @@ git am ../patch/0001-powerpc-enabled-building-with-CONFIG_DM-y.patch
 git am ../patch/0001-test-call-hexdump-tests-via-ut-lib.patch
 git am ../patch/0001-fs-fat-fix-link-error-when-building-with-DEBUG-1.patch
 git am ../patch/0001-cmd-set-CONFIG_SYS_HELP_CMD_WIDTH-10.patch
+git am ../patch/0001-efi_loader-do-not-add-in-efi_fs_from_name.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
