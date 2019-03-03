74ee84c7d0cb26f08bd40be55eaa061112b25812
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-03-03 10:47:46 +0100

    patch-ipxe.sh
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-ipxe.sh
+++ patch-ipxe.sh
@@ -2,5 +2,9 @@
 git am --abort || true
 
 git am ../patch/0001-build-Don-t-use-mabi-lp64-for-HOSTCC.patch
-git am ../patch/0001-efi-avoid-stringop-truncation-error-in-util-elf2efi..patch
+MK_ARCH=`shell uname -m`
+if [ "aarch64" = "$MK_ARCH" ]
+then
+    git am ../patch/0001-efi-avoid-stringop-truncation-error-in-util-elf2efi..patch
+fi
 # git am ../patch/0001-build-Fix-building-with-gcc-8.2.patch
