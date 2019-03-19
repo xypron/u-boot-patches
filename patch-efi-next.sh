c40c071499b5ac2de1819115c83f109d0bfa6f4d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-03-19 20:13:01 +0100

    0001-efi_selftest-fix-test_hii_string_get_string.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -55,6 +55,7 @@ git am ../patch/0001-efi_loader-endless-loop-in-add_strings_package.patch
 git am ../patch/0001-efi_loader-missing-return-in-efi_get_next_variable_n.patch
 git am ../patch/0001-efi_loader-memory-leak-in-efi_dump_single_var.patch
 git am ../patch/0001-efi_loader-superfluous-conversion-in-efi_file_open.patch
+git am ../patch/0001-efi_selftest-fix-test_hii_string_get_string.patch
 
 exit
 
