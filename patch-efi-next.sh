7571d0e86753d3ad4a4fee2a5dc3f96b98e8a7f5
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-04-19 06:04:33 +0200

    0001-cmd-bootefi-merge-efi_install_fdt-and-efi_process_fd.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -59,6 +59,7 @@ git am ../patch/0001-efi_loader-efi_setup_loaded_image-handle-missing-fil.patch
 git am ../patch/0001-efi_loader-export-root-node-handle.patch
 git am ../patch/0001-cmd-bootefi-rework-set_load_options.patch
 git am ../patch/0001-cmd-bootefi-carve-out-fdt-handling-from-do_bootefi.patch
+git am ../patch/0001-cmd-bootefi-merge-efi_install_fdt-and-efi_process_fd.patch
 exit
 git am ../patch/0001-efi_loader-remove-relocation-header.patch
 exit
