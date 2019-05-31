661383afae0be8bacc553ebb267585bdd4c6bf1e
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-05-31 23:00:20 +0200

    0001-efi_loader-Kconfig-entries-for-GetTime-SetTime.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -73,6 +73,7 @@ git am ../patch/0001-efi_loader-check-time-in-SetTime.patch
 git am ../patch/0001-efi_loader-export-efi_set_time.patch
 # efi-2019-07-rc4-2
 git am ../patch/0001-efi_loader-handling-of-daylight-saving-time.patch
+git am ../patch/0001-efi_loader-Kconfig-entries-for-GetTime-SetTime.patch
 exit
 git am ../patch/0001-efi_loader-use-upper-case-for-GUIDs.patch
 git am ../patch/0001-efi_loader-remove-relocation-header.patch
