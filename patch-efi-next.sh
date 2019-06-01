2d57bb12ef8332ad06dd184ba53503981cd9d3ab
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-06-01 20:24:10 +0200

    0001-efi_loader-open-protocol-information.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -76,6 +76,7 @@ git am ../patch/0001-efi_loader-Kconfig-entries-for-GetTime-SetTime.patch
 # https://travis-ci.org/xypron2/u-boot/builds/539972198
 # efi-2019-07-rc4-2
 git am ../patch/0001-efi_loader-correct-HandleProtocol.patch
+git am ../patch/0001-efi_loader-open-protocol-information.patch
 exit
 git am ../patch/0001-efi_loader-use-upper-case-for-GUIDs.patch
 git am ../patch/0001-efi_loader-remove-relocation-header.patch
