019db5d6c0e9c0a3d0b078d6166d0833b53f0454
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-06-01 21:00:31 +0200

    0001-efi_loader-CloseProtocol-fix-open-protocol-informati.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -77,6 +77,7 @@ git am ../patch/0001-efi_loader-Kconfig-entries-for-GetTime-SetTime.patch
 # efi-2019-07-rc4-2
 git am ../patch/0001-efi_loader-correct-HandleProtocol.patch
 git am ../patch/0001-efi_loader-open-protocol-information.patch
+git am ../patch/0001-efi_loader-CloseProtocol-fix-open-protocol-informati.patch
 exit
 git am ../patch/0001-efi_loader-use-upper-case-for-GUIDs.patch
 git am ../patch/0001-efi_loader-remove-relocation-header.patch
