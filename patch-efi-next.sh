25733b0a2c70e514bae56b47f2910c92191df0d3
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-06-04 21:18:55 +0200

    0001-efi_loader-notify-memory-map-changes.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -69,9 +69,9 @@ git am ../patch/0001-lib-time-export-usec_to_tick.patch
 git am ../patch/0001-efi_loader-check-timer-events-in-Stall.patch
 git am ../patch/0001-efi_loader-fix-EnableCursor.patch
 git am ../patch/0001-efi_loader-close-protocols-in-UnloadImage.patch
+git am ../patch/0001-efi_loader-notify-memory-map-changes.patch
 git am ../patch/0001-efi_loader-signal-events-in-order-of-decreasing-TPL.patch
 git am ../patch/0001-efi_loader-call-event-function-at-event-TPL.patch
-git am ../patch/0001-efi_loader-notify-memory-map-changes.patch
 exit
 git am ../patch/0001-efi_loader-use-upper-case-for-GUIDs.patch
 git am ../patch/0001-efi_loader-remove-relocation-header.patch
