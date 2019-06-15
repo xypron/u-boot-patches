d85377ef827d33521bbaf88b4cf296897f50cc94
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-06-15 14:52:14 +0200

    0001-efi_loader-system-table-setup.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -70,6 +70,7 @@ git am ../patch/0001-efi_loader-QueryMode-check-parameters.patch
 git am ../patch/0001-efi_loader-SetMode-parameters-check.patch
 git am ../patch/0001-efi_loader-QueryMode-must-allocate-buffer.patch
 git am ../patch/0001-efi_loader-EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL-definitio.patch
+git am ../patch/0001-efi_loader-system-table-setup.patch
 exit
 git am ../patch/0001-efi_loader-use-upper-case-for-GUIDs.patch
 git am ../patch/0001-efi_loader-remove-relocation-header.patch
