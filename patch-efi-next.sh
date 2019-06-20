b54775d7c93999ae0695f5fce94827960ffdaa22
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-06-20 15:45:55 +0200

    0001-efi_loader-initialization-of-variable-services.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -69,7 +69,7 @@ git am ../patch/0001-efi_loader-move-efi_query_variable_info.patch
 git am ../patch/0001-efi_loader-consistent-types-in-efidebug.c.patch
 git am ../patch/0001-efi_loader-consistent-error-handling-in-efidebug.c.patch
 git am ../patch/0001-efi_debug-make-variable-support-customizable.patch
-git am ../patch/0001-efi_loader-initalization-of-variable-services.patch
+git am ../patch/0001-efi_loader-initialization-of-variable-services.patch
 git am ../patch/0001-efi_loader-let-the-variable-driver-patch-out-the-run.patch
 git am ../patch/0001-efi_loader-unimplemented-runtime-services.patch
 exit
