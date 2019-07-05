43a4c4f0cb3f1dbc6c2444f46523f44f7736acc5
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-05 21:42:35 +0200

    0001-disk-efi-buffer-overflow-in-part_get_info_efi.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -57,6 +57,7 @@ git am ../patch/0001-ARM-defconfig-add-Raspberry-Pi-3-Model-B.patch
 git am ../patch/0001-arm-qemu-fix-failure-in-flash-initialization-if-boot.patch
 
 # efi-2019-10
+git am ../patch/0001-disk-efi-buffer-overflow-in-part_get_info_efi.patch
 git am ../patch/0001-efi_loader-remove-superfluous-spaces-in-comments.patch
 git am ../patch/0001-efi_loader-move-efi_query_variable_info.patch
 git am ../patch/0001-efi_loader-initialization-of-variable-services.patch
