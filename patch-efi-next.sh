5d169301c0dab5200b5078622055f39b1e63a2d0
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-11 06:12:04 +0200

    0001-efi_loader-duplicate-IMAGE_SUBSYSTEM_EFI_APPLICATION.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -52,6 +52,7 @@ git am ../patch/0001-ARM-correct-detection-of-thumb-mode.patch
 git am ../patch/0001-arm-qemu-fix-failure-in-flash-initialization-if-boot.patch
 
 # efi-2019-10-rc1-2
+git am ../patch/0001-efi_loader-duplicate-IMAGE_SUBSYSTEM_EFI_APPLICATION.patch
 git am ../patch/0001-efi_loader-protocol-definitions-for-ConnectControlle.patch
 exit
 git am ../patch/0001-efi_loader-implement-reading-volume-label.patch
