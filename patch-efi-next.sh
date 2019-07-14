5c5cf751e8718ceb1f8e585657ddaab6ab2b0c95
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-14 11:32:58 +0200

    0001-scripts-kernel-doc-fix-parsing-of-function-pointers.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -21,6 +21,8 @@ git am ../patch/0001-fs-fat-validate-sector-and-cluster-size.patch
 git am ../patch/0001-arm64-mvebu-armada-8k-support-booting-from-NVME.patch
 git am ../patch/0001-cmd-usb-display-bus-number.patch
 git am ../patch/0001-doc-Cope-with-Sphinx-logging-deprecations.patch
+git am ../patch/0001-scripts-kernel-doc-update-script-from-Linux-5.2.patch
+git am ../patch/0001-scripts-kernel-doc-fix-parsing-of-function-pointers.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
