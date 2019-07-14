16da6c453aed0abc73cf22710ee10ee2067a9f43
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-14 13:03:51 +0200

    0001-efi_loader-fix-function-comments-in-cmd-bootefi.c.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -67,6 +67,7 @@ git am ../patch/0001-efi_loader-fix-comments-for-efi_update_exit_data.patch
 git am ../patch/0001-efi_driver-use-Sphinx-style-comments.patch
 git am ../patch/0001-efi_loader-fix-comments-for-variable-services.patch
 git am ../patch/0001-efi_loader-fix-comment-for-struct-efi_pool_allocatio.patch
+git am ../patch/0001-efi_loader-fix-function-comments-in-cmd-bootefi.c.patch
 exit
 git am ../patch/0001-efi_loader-protocol-definitions-for-ConnectControlle.patch
 git am ../patch/0001-efi_loader-implement-reading-volume-label.patch
