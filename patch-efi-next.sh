ad1a8a0f723d4ae0c07f343ea678d9877ace5183
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-14 20:18:29 +0200

    0001-efi_loader-unaligned-access-in-efi_file_from_path.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -71,6 +71,7 @@ git am ../patch/0001-efi_loader-avoid-alignment-error.patch
 git am ../patch/0001-efi_loader-support-unaligned-u16-strings.patch
 git am ../patch/0001-test-unit-test-for-u16_strlen.patch
 git am ../patch/0001-efi_loader-efi_dp_from_file-expect-UTF-8-path.patch
+git am ../patch/0001-efi_loader-unaligned-access-in-efi_file_from_path.patch
 exit
 git am ../patch/0001-efi_loader-protocol-definitions-for-ConnectControlle.patch
 git am ../patch/0001-efi_loader-implement-reading-volume-label.patch
