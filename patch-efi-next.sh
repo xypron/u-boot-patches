c42dea019ca9e23f885be8e0fe5440b3d65f0254
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-27 20:13:14 +0200

    0001-efi_selftest-sharpen-ConvertPointer-test.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -54,6 +54,7 @@ git am ../patch/0001-efi_loader-always-rebuild-efi_crt0.o.patch
 git am ../patch/0001-efi_loader-efidebug.c-function-documentation.patch
 git am ../patch/0001-doc-integrate-UEFI-documentation-into-Sphinx-toctree.patch
 git am ../patch/0001-efi_selftest-fix-SetVirtualAddressMap-unit-test.patch
+git am ../patch/0001-efi_selftest-sharpen-ConvertPointer-test.patch
 exit
 
 git am ../patch/0001-efi_loader-remove-efi_exit_caches.patch
