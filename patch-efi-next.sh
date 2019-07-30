6e292d0a42acbf285377779b081d9e85b36da7a5
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-07-30 23:04:16 +0200

    0001-clk-meson-remove-duplicate-logic.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -19,6 +19,7 @@ git am ../patch/0001-fs-fat-validate-sector-and-cluster-size.patch
 git am ../patch/0001-arm64-mvebu-armada-8k-support-booting-from-NVME.patch
 git am ../patch/0001-cmd-usb-display-bus-number.patch
 git am ../patch/0001-log-document-the-assign-macro.patch
+git am ../patch/0001-clk-meson-remove-duplicate-logic.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
