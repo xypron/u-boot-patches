0886611425fef36d2505929913c233ec9ba09e32
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-08-09 18:04:11 +0000

    Remove accepted patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -18,7 +18,6 @@ git am ../patch/0001-dm-sysreset-x86-missing-build-dependency.patch
 git am ../patch/0001-fs-fat-validate-sector-and-cluster-size.patch
 git am ../patch/0001-arm64-mvebu-armada-8k-support-booting-from-NVME.patch
 git am ../patch/0001-cmd-usb-display-bus-number.patch
-git am ../patch/0001-log-document-the-assign-macro.patch
 git am ../patch/0001-test-dm_mdio-avoid-out-of-bounds-access.patch
 git am ../patch/0001-drivers-net-pfe_eth-undefined-return-value.patch
 git am ../patch/0001-bios_emulator-remove-stray-break.patch
