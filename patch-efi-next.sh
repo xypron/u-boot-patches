99245a831e7ac7c58b98de7de4e92705fab2517d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-08-14 07:00:19 +0200

    0001-efi_loader-do-not-call-efi_runtime_detach-twice.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -50,10 +50,11 @@ git am ../patch/0001-disk-part-rename-parameter-of-lba512_muldiv.patch
 # RISC-V
 git am ../patch/0001-riscv-qemu-enable-CONFIG_CMD_BOOTEFI_SELFTEST.patch
 
-# efi-2019-10-rc2-2
+# efi-2019-10-rc3
 git am ../patch/0001-efi_loader-EFI_HII_DATABASE_PROTOCOL.SetKeyboardLayo.patch
 git am ../patch/0001-efi_loader-EFI_PXE_BASE_CODE_PROTOCOL-stub.patch
 git am ../patch/0001-efi_loader-parameter-check-in-SetVirtualAddressMap.patch
+git am ../patch/0001-efi_loader-do-not-call-efi_runtime_detach-twice.patch
 exit
 
 git am ../patch/0001-efi_loader-remove-efi_exit_caches.patch
