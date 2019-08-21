b6b2641ada333986dafad9bf394f9b11275b9464
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-08-21 20:29:19 +0200

    0001-tools-remove-easylogo-and-include-video_logo.h.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -21,6 +21,7 @@ git am ../patch/0001-cmd-usb-display-bus-number.patch
 git am ../patch/0001-test-dm_mdio-avoid-out-of-bounds-access.patch
 git am ../patch/0001-drivers-net-pfe_eth-undefined-return-value.patch
 git am ../patch/0001-cmd-bdinfo-sandbox-print-the-relocation-offset.patch
+git am ../patch/0001-tools-remove-easylogo-and-include-video_logo.h.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
