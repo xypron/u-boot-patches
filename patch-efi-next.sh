376b62b0906527dd88876fe8347f881370c59bba
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-10-06 14:01:12 +0200

    0001-lib-errno-avoid-error-format-overflow.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -18,6 +18,7 @@ git am ../patch/0001-cmd-usb-display-bus-number.patch
 git am ../patch/0001-cmd-host-fix-seg-fault-at-host-info.patch
 git am ../patch/0001-arm64-print-instructions-leading-to-exception.patch
 git am ../patch/0001-lib-errno-sync-error-codes.patch
+git am ../patch/0001-lib-errno-avoid-error-format-overflow.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
