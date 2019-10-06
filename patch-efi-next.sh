5da5421887a9ff7903290df12a7dc2b3352f8e3e
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-10-06 23:17:28 +0200

    0001-tools-provide-cbfstool.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -22,6 +22,7 @@ git am ../patch/0001-lib-errno-avoid-error-format-overflow.patch
 git am ../patch/0001-lib-errno-check-for-unsupported-error-number.patch
 git am ../patch/0001-test-provide-test-for-errno_str.patch
 git am ../patch/0001-configs-sheevaplug-disable-EFI_UNICODE_CAPITALIZATIO.patch
+git am ../patch/0001-tools-provide-cbfstool.patch
 
 # USB keyboard
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
