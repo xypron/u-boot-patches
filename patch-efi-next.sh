cc524f17b7f54749f9ccb839b1574108f90ab148
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-11-23 08:43:41 +0100

    0001-usb-kbd-reduce-code-size.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -23,6 +23,7 @@ git am ../patch/0001-usb-dwc2-fix-possible-alignment-issues.patch
 # git am ../patch/0001-input-add-PS-2-keyboard-support.patch
 git am ../patch/0001-usb-kbd-simplify-coding-for-arrow-keys.patch
 git am ../patch/0001-usb-kbd-signature-of-usb_kbd_put_queue.patch
+git am ../patch/0001-usb-kbd-reduce-code-size.patch
 git am ../patch/0001-usb-kbd-implement-special-keys.patch
 git am ../patch/0001-usb-kbd-fix-typo.patch
 git am ../patch/0001-usb-kbd-move-USB_KBD_BOOT_REPORT_SIZE-to-usb.h.patch
