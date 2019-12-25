3e83a91f7d9bfa9ffa336b7a6a4aa5c9999fac92
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-12-25 02:40:20 +0100

    0001-cmd-add-rng-command.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -71,6 +71,7 @@ git am ../patch/0001-sandbox-rng-Add-a-random-number-generator-rng-driver.patch
 git am ../patch/0001-configs-sandbox-Enable-random-number-generator-rng-d.patch
 git am ../patch/0001-test-rng-Add-basic-test-for-random-number-generator-.patch
 git am ../patch/0001-virtio-rng-Add-a-random-number-generator-rng-driver.patch
+git am ../patch/0001-cmd-add-rng-command.patch
 exit
 
 git am ../patch/0001-efi_loader-EFI_FILE_PROTOCOL-rev-2-stub.patch
