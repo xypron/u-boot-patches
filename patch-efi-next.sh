cfefb2272ff0c9c1bffc45379c823b69f56b267d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-12-28 15:55:35 +0100

    0001-efi_loader-remove-__packed-from-struct-efi_configura.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -53,6 +53,7 @@ git am ../patch/0001-efi_loader-git-ignore-helloworld_efi.S.patch
 git am ../patch/0001-test-py-describe-env__efi_loader_helloworld_file.patch
 git am ../patch/0001-efi_loader-clear-screen-should-move-cursor-to-home.patch
 git am ../patch/0001-part-efi-comment-for-GPT_HEADER_SIGNATURE_UBOOT.patch
+git am ../patch/0001-efi_loader-remove-__packed-from-struct-efi_configura.patch
 exit
 
 # UEFI FIT image
