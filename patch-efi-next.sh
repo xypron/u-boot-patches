46bc93e5dc7c1cdafdc44b7f9a165fbcf08a313b
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-01-08 08:03:05 +0100

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -38,8 +38,6 @@ git am ../patch/0001-net-sun8i_emac-increase-_sun8i_emac_eth_init-timeout.patch
 git am ../patch/0001-disk-part-rename-parameter-of-lba512_muldiv.patch
 
 # Sandbox
-git am ../patch/0001-sandbox-add-missing-compatible-property-in-device-tr.patch
-git am ../patch/0001-serial-sandbox-support-Unicode.patch
 git am ../patch/0001-configs-sandbox-enable-CONFIG_CMD_BOOTEFI_SELFTEST.patch
 
 #
