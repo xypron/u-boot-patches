b3541ed3f98b5c688d27204edc931bff467888d0
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-01-10 12:43:59 +0100

    0001-efi_loader-link-partition-to-block-device.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -50,6 +50,8 @@ git am ../patch/0001-configs-sandbox-enable-CONFIG_CMD_BOOTEFI_SELFTEST.patch
 # UEFI
 git am ../patch/0001-efi_loader-document-functions-in-efi_rng.c.patch
 git am ../patch/0001-efi_driver-fix-efi_uc_stop.patch
+git am ../patch/0001-efi_loader-make-efi_protocol_open-non-static.patch
+git am ../patch/0001-efi_loader-link-partition-to-block-device.patch
 exit
 
 git am ../patch/0001-efi_loader-EFI_FILE_PROTOCOL-rev-2-stub.patch
