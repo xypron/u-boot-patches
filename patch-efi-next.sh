790372b0d43d4d1380657c7cef63380463d2db65
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-13 02:47:48 +0100

    0001-configs-sandbox-enable-LOG_SYSLOG.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -47,6 +47,7 @@ git am ../patch/0001-configs-sandbox-enable-CONFIG_CMD_BOOTEFI_SELFTEST.patch
 git am ../patch/0001-drivers-rng-simplify-Kconfig.patch
 git am ../patch/0001-drivers-rng-add-Amlogic-hardware-RNG-driver.patch
 
+# Logging
 git am ../patch/0001-net-designware-speed-should-be-in-a-debug-message.patch
 git am ../patch/0001-log-correct-CONFIG_LOG_TEST-prerequisites.patch
 git am ../patch/0001-log-syslog-driver.patch
@@ -54,6 +55,7 @@ git am ../patch/0001-common-console.c-discard-volatile.patch
 git am ../patch/0001-log-output-for-CONFIG_LOG-n.patch
 git am ../patch/0001-test-log-functions-with-CONFIG_LOG-n.patch
 git am ../patch/0001-test-log-test-syslog-logging-driver.patch
+git am ../patch/0001-configs-sandbox-enable-LOG_SYSLOG.patch
 
 # UEFI
 git am ../patch/0001-efi_loader-fix-efi_install_fdt-description.patch
