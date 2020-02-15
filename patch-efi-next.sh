e67bc4c89285c2bf015fe0b387499c764932b5f4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-15 21:59:02 +0100

    0001-arm-mvebu-drivers-ddr-remove-redundant-assignment.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -25,6 +25,7 @@ git am ../patch/0001-clk-meson-g12a-missing-break.patch
 git am ../patch/0001-x86-remove-dead-code-in-intel_clk_get_rate.patch
 git am ../patch/0001-clk-ICS8N3QV01-remove-superfluous-code.patch
 git am ../patch/0001-dm-core-remove-redundant-if-statement.patch
+git am ../patch/0001-arm-mvebu-drivers-ddr-remove-redundant-assignment.patch
 
 # make htmldocs
 git am ../patch/0001-doc-Makefile-turn-warnings-into-errors.patch
