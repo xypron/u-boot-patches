3febd1aa416cf63dd3ec41283e0407ceada49087
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-15 21:11:28 +0100

    0001-clk-meson-g12a-missing-break.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -21,6 +21,7 @@ git am ../patch/0001-net-tftp-use-correct-printf-codes.patch
 git am ../patch/0001-cli-allow-verbatim-character-entry-with-CTRL-v.patch
 git am ../patch/0001-test-verbatim-character-entry-with-CTRL-V.patch
 git am ../patch/0001-watchdog-cannot-select-CONFIG_CONFIG_MPC8xx_WATCHDOG.patch
+git am ../patch/0001-clk-meson-g12a-missing-break.patch
 
 # make htmldocs
 git am ../patch/0001-doc-Makefile-turn-warnings-into-errors.patch
