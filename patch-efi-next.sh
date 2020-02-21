feeb4eea6e6c865707d0893b045c3ed718c017a1
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-21 06:33:36 +0100

    0001-azure-build-HTML-documentation.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -31,6 +31,7 @@ git am ../patch/0001-arm-mvebu-drivers-ddr-remove-redundant-assignment.patch
 git am ../patch/0001-doc-Makefile-turn-warnings-into-errors.patch
 git am ../patch/0001-travis-build-HTML-docs.patch
 git am ../patch/0001-gitlab-build-HTML-documentation.patch
+git am ../patch/0001-azure-build-HTML-documentation.patch
 
 # sct
 # git am ../patch/0001-efi_loader-provide-PEI-services-table.patch
