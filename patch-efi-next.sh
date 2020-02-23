ce76feadbc1c793fc056577464d585bdbbb5c662
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-23 12:59:16 +0100

    0001-efi_loader-pytest-set-up-secure-boot-environment.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -81,6 +81,7 @@ git am ../patch/0001-efi_loader-add-CONFIG_EFI_SECURE_BOOT-config-option.patch
 git am ../patch/0001-cmd-env-use-appropriate-guid-for-authenticated-UEFI-.patch
 git am ../patch/0001-cmd-env-add-at-option-to-env-set-e-command.patch
 git am ../patch/0001-cmd-efidebug-add-test-bootmgr-sub-command.patch
+git am ../patch/0001-efi_loader-pytest-set-up-secure-boot-environment.patch
 exit
 
 git am ../patch/0001-efi_loader-architecture-specific-UEFI-setup.patch
