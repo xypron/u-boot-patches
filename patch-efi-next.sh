332e383a64d20d1fb0550982b3912bdbf9534e51
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-23 12:55:03 +0100

    0001-cmd-env-use-appropriate-guid-for-authenticated-UEFI-.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -78,6 +78,7 @@ git am ../patch/0001-efi_loader-variable-add-VendorKeys-variable.patch
 git am ../patch/0001-efi_loader-image_loader-support-image-authentication.patch
 git am ../patch/0001-efi_loader-set-up-secure-boot.patch
 git am ../patch/0001-efi_loader-add-CONFIG_EFI_SECURE_BOOT-config-option.patch
+git am ../patch/0001-cmd-env-use-appropriate-guid-for-authenticated-UEFI-.patch
 exit
 
 git am ../patch/0001-efi_loader-architecture-specific-UEFI-setup.patch
