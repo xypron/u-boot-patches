d0129078952023e4a6f38484a7e64e1da68e23d8
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-25 08:02:18 +0100

    0001-efi_loader-add-some-description-about-UEFI-secure-bo.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -79,6 +79,7 @@ git am ../patch/0001-efi_loader-variable-add-VendorKeys-variable.patch
 git am ../patch/0001-efi_loader-image_loader-support-image-authentication.patch
 git am ../patch/0001-efi_loader-set-up-secure-boot.patch
 git am ../patch/0001-efi_loader-add-CONFIG_EFI_SECURE_BOOT-config-option.patch
+git am ../patch/0001-efi_loader-add-some-description-about-UEFI-secure-bo.patch
 git am ../patch/0001-cmd-env-use-appropriate-guid-for-authenticated-UEFI-.patch
 git am ../patch/0001-cmd-env-add-at-option-to-env-set-e-command.patch
 git am ../patch/0001-cmd-efidebug-add-test-bootmgr-sub-command.patch
