4c5e8f295c9c5555dfd371e8e2406ec8c6c7d467
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-02-25 21:36:19 +0100

    0001-doc-fix-references-to-driver-model.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -28,6 +28,7 @@ git am ../patch/0001-dm-core-remove-redundant-if-statement.patch
 git am ../patch/0001-arm-mvebu-drivers-ddr-remove-redundant-assignment.patch
 git am ../patch/0001-README-replace-reference-to-boards.cfg.patch
 git am ../patch/0001-scripts-add-documentation-file-ref-check.patch
+git am ../patch/0001-doc-fix-references-to-driver-model.patch
 
 # sct
 # git am ../patch/0001-efi_loader-provide-PEI-services-table.patch
