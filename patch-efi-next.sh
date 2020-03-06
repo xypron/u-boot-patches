e1b5ef05de3a8dc8458334a0dbf734fc58dbbbed
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-03-06 22:43:56 +0100

    0001-efi_loader-unnecessary-assignment-in-efi_queue_event.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -69,6 +69,7 @@ git am ../patch/0001-configs-sandbox-enable-LOG_SYSLOG.patch
 # UEFI
 git am ../patch/0001-cmd-efidebug-fix-a-failure-of-boot-rm-sub-command.patch
 git am ../patch/0001-cmd-efidebug-correct-error-message.patch
+git am ../patch/0001-efi_loader-unnecessary-assignment-in-efi_queue_event.patch
 exit
 
 # UEFI secure boot
