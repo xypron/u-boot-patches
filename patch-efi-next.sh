89ab35fcb8f7bdc7c14b5665e900ee5409c710da
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-03-22 09:08:49 +0100

    0001-efi_loader-memory-buffer-for-variables.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -82,6 +82,7 @@ git am ../patch/0001-efi_loader-export-initialization-state.patch
 git am ../patch/0001-efi_loader-change-setup-sequence.patch
 git am ../patch/0001-efi_loader-imply-FAT-FAT_WRITE.patch
 git am ../patch/0001-efi_loader-UEFI-variable-persistence.patch
+git am ../patch/0001-efi_loader-memory-buffer-for-variables.patch
 exit
 
 # UEFI secure boot
