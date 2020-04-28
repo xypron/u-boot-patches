9b1a1e2b920b73b9eb91073c5356b56046c35e02
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-04-28 21:56:55 +0200

    0001-hush-avoid-NULL-check-before-free.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -34,6 +34,7 @@ git am ../patch/0001-Add-.readthedocs.yml.patch
 git am ../patch/0001-doc-describe-the-analysis-of-crash-dumps.patch
 git am ../patch/0001-cmd-gpt-avoid-NULL-check-before-free.patch
 git am ../patch/0001-fs-ext4-avoid-NULL-check-before-free.patch
+git am ../patch/0001-hush-avoid-NULL-check-before-free.patch
 
 # sct
 # git am ../patch/0001-efi_loader-provide-PEI-services-table.patch
