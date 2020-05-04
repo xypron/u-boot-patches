337ddead48ea8e566b7eed6e4451dce5622795c2
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-05-04 12:25:24 +0200

    0001-test-stabilize-test_efi_secboot.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -44,6 +44,7 @@ git am ../patch/0001-cmd-host-return-value-of-do_host_bind.patch
 git am ../patch/0001-net-designware-speed-should-be-in-a-debug-message.patch
 
 # UEFI
+# efi-2020-07-rc2
 git am ../patch/0001-lib-crypto-efi_loader-avoid-multiple-inclusions-of-h.patch
 git am ../patch/0001-lib-crypto-efi_loader-move-some-headers-to-include-c.patch
 git am ../patch/0001-efi_loader-fix-unreachable-statement-in-efi_sigstore.patch
@@ -53,9 +54,7 @@ git am ../patch/0001-cmd-efidebug-simplify-UEFI-protocol-calls.patch
 git am ../patch/0001-efi_loader-eliminate-efi_get_-non-volatile_variable.patch
 git am ../patch/0001-efi_loader-eliminate-efi_set_-non-volatile_variable.patch
 git am ../patch/0001-efi_loader-correct-comments-for-efi_status_t.patch
-
-# efi-2020-07
-exit
+git am ../patch/0001-test-stabilize-test_efi_secboot.patch
 git am ../patch/0001-efi_loader-change-setup-sequence.patch
 
 exit
