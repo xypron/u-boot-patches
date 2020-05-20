0c8fe16ecc256129709ebdbdad740b7a5658dc94
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-05-20 21:32:41 +0200

    0001-efi_loader-initialize-root-node-first.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -49,6 +49,7 @@ git am ../patch/0001-net-designware-speed-should-be-in-a-debug-message.patch
 # efi-2020-07-rc3-2
 git am ../patch/0001-efi_loader-Remove-unnecessary-debug.patch
 git am ../patch/0001-efi_loader-add-EFI_MEMORY_SP-to-memory-attributes.patch
+git am ../patch/0001-efi_loader-initialize-root-node-first.patch
 
 exit
 git am ../patch/0001-efi_loader-eliminate-EFI_CALL-for-variable-access.patch
