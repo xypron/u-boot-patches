bc2f4ec7d604b1d6051e783097086682653fe5a2
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-05-30 06:48:03 +0200

    0001-efi_loader-function-description-cmp_pe_section.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -40,6 +40,7 @@ git am ../patch/0001-arm-use-correct-argument-size-of-special-registers.patch
 # efi-2020-07-rc4
 git am ../patch/0001-efi_loader-allow-compiling-with-clang.patch
 git am ../patch/0001-efi_loader-function-descriptions-efi_image_loader.c.patch
+git am ../patch/0001-efi_loader-function-description-cmp_pe_section.patch
 git am ../patch/0001-efi_loader-signature-move-efi_guid_cert_type_pkcs7-t.patch
 
 exit
