df54f01050a0ef15b4e27c121fd7ccf320023e9e
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-06-06 10:09:46 +0000

    rm 0001-config-remove-CONFIG_ARMV8_SWITCH_TO_EL1-from-whitel.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -29,7 +29,6 @@ git am ../patch/0001-log-check-argument-of-log-level-command.patch
 git am ../patch/0001-log-don-t-show-function-by-default.patch
 git am ../patch/0001-smbios-empty-strings-in-smbios_add_string.patch
 git am ../patch/0001-sunxi-Pine64-LTS-SMBIOS-properties.patch
-git am ../patch/0001-config-remove-CONFIG_ARMV8_SWITCH_TO_EL1-from-whitel.patch
 git am ../patch/0001-sandbox-handling-out-of-memory.patch
 git am ../patch/0001-doc-move-Clang-documentation-to-HTML.patch
 
