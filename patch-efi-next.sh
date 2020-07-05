f871a8f5ea8435086a02710c2f1e6e87cedd8d5d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-07-05 17:56:55 +0200

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -32,9 +32,6 @@ git am ../patch/0001-sunxi-Pine64-LTS-SMBIOS-properties.patch
 git am ../patch/0001-doc-move-Clang-documentation-to-HTML.patch
 git am ../patch/0001-cmd-fdt-remove-CMD_FDT_MAX_DUMP.patch
 git am ../patch/0001-gitlab-show-skipped-Python-tests.patch
-git am ../patch/0001-riscv-Do-not-return-error-if-reserved-node-already-e.patch
-git am ../patch/0001-riscv-Use-optimized-version-of-fdtdec_get_addr_size_.patch
-git am ../patch/0001-riscv-use-log-functions-in-fdt_fixup.patch
 git am ../patch/0001-ARM-mx6-make-CAAM-usable-on-the-i.MX6-boards.patch
 git am ../patch/0001-crypto-fsl-correct-printf-statement.patch
 git am ../patch/0001-crypto-fsl-unused-value-in-caam_hash_update.patch
