3a9311dea0092e7c020bd3e06660439eac2dad5d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-07-13 22:26:20 +0200

    0001-cmd-fix-lsblk-command.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -29,6 +29,7 @@ git am ../patch/0001-crypto-fsl-unused-value-in-caam_hash_update.patch
 git am ../patch/0001-fs-error-handling-in-do_load.patch
 git am ../patch/0001-doc-correct-description-of-crash-dumps.patch
 git am ../patch/0001-.gitlab-ci.yml-copy-GRUB-to-correct-build-path.patch
+git am ../patch/0001-cmd-fix-lsblk-command.patch
 
 git am ../patch/0001-m68k-define-__raw_writeq-__raw_readq.patch
 git am ../patch/0001-microblaze-define-__raw_writeq-__raw_readq.patch
