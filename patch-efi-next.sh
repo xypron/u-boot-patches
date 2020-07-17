b1ba37ffe63ae8a75caf22a64ec11bb26cf7862a
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-07-17 20:35:41 +0200

    0001-efi_loader-use-logging-for-block-device-messages.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -55,6 +55,7 @@ git am ../patch/0001-examples-do-not-build-standalone-applications-with-F.patch
 
 # efi-2020-10-rc1-5
 git am ../patch/0001-efi_loader-use-logging-for-bootefi-command.patch
+git am ../patch/0001-efi_loader-use-logging-for-block-device-messages.patch
 git am ../patch/0001-efi_loader-Rename-and-correct-values-for-ARM_SMC_MM_.patch
 git am ../patch/0001-lib-crypto-add-public_key_verify_signature.patch
 git am ../patch/0001-lib-crypto-enable-x509_check_for_self_signed.patch
