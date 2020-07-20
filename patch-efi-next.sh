644f2de441c52c74e0afccd6d8b0fe7706fa141c
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-07-20 12:48:57 +0200

    0001-test-py-efi_secboot-remove-unused-function.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -56,6 +56,8 @@ git am ../patch/0001-efi_loader-returning-from-UEFI-FIT-images.patch
 git am ../patch/0001-efi_loader-use-logging-for-bootefi-command.patch
 git am ../patch/0001-efi_loader-use-logging-for-block-device-messages.patch
 git am ../patch/0001-efi_loader-Rename-and-correct-values-for-ARM_SMC_MM_.patch
+git am ../patch/0001-test-py-efi_secboot-remove-unused-function.patch
+git am ../patch/0001-test-py-efi_secboot-fix-additional-pylint-errors.patch
 git am ../patch/0001-lib-crypto-add-public_key_verify_signature.patch
 git am ../patch/0001-lib-crypto-enable-x509_check_for_self_signed.patch
 git am ../patch/0001-lib-crypto-import-pkcs7_verify.c-from-linux.patch
