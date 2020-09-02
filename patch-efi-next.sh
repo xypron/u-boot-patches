efaec5be3f36dbea020520a4d894fee78bd0e2c6
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-09-02 08:13:53 +0200

    0001-efi_loader-consider-no-map-property-of-reserved-memo.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -63,7 +63,6 @@ git am ../patch/0001-fs-convert-error-and-debug-messages-to-log.patch
 git am ../patch/0001-efi_loader-log-messages-for-bootefi-command.patch
 git am ../patch/0001-efi_loader-log-function-in-image-loader.patch
 git am ../patch/0001-efi_loader-error-message-if-image-not-authenticated.patch
-git am ../patch/0001-efi_loader-consider-no-map-property-of-reserved-memo.patch
 git am ../patch/0001-efi-clean-up-efi-command.patch
 git am ../patch/0001-efi_loader-remove-duplicate-image-size-check.patch
 git am ../patch/0001-efi_selftest-simplify-Makefile.patch
