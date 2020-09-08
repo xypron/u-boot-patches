615af5fa5303c76d68a7ee9a12ab986e7696e5c8
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-09-08 08:10:40 +0200

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -63,14 +63,7 @@ git am ../patch/0001-examples-do-not-build-standalone-applications-with-F.patch
 
 # UEFI
 
-# efi-2020-10-rc4
-git am ../patch/0001-fs-convert-error-and-debug-messages-to-log.patch
-git am ../patch/0001-efi_loader-log-messages-for-bootefi-command.patch
-git am ../patch/0001-efi_loader-log-function-in-image-loader.patch
-git am ../patch/0001-efi_loader-error-message-if-image-not-authenticated.patch
-git am ../patch/0001-efi-clean-up-efi-command.patch
-git am ../patch/0001-efi_loader-remove-duplicate-image-size-check.patch
-git am ../patch/0001-efi_selftest-simplify-Makefile.patch
+# efi-2020-10-rc5
 exit
 
 # efi-2021-01
