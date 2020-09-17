84cb2111cf87ac81027a87f63445af701e0b3caf
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-09-17 09:27:52 +0200

    0001-efi_memory-refine-overlap_only_ram-description.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -69,6 +69,7 @@ git am ../patch/0001-examples-do-not-build-standalone-applications-with-F.patch
 # UEFI
 
 # efi-2020-10-rc5
+git am ../patch/0001-efi_memory-refine-overlap_only_ram-description.patch
 git am ../patch/0001-efi_selftest-rework-device-tree-test.patch
 git am ../patch/0001-efi_selftest-check-for-RISC-V-boot-hartid-in-FDT.patch
 exit
