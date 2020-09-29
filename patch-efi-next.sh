a457aed8128c6d52e9ad8e57b5d575a70fae49a2
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-09-29 19:47:18 +0200

    0001-sandbox-redefine-getc.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -79,7 +79,7 @@ git am ../patch/0001-mtd-cfi_flash-read-device-tree-correctly.patch
 # Sandbox
 # git am ../patch/0001-configs-sandbox-enable-CONFIG_CMD_BOOTEFI_SELFTEST.patch
 git am ../patch/0001-sandbox-add-missing-definitions-for-flash-drivers.patch
-git am ../patch/0001-common-redefine-getc.patch
+git am ../patch/0001-sandbox-redefine-getc.patch
 git am ../patch/0001-sandbox-add-missing-SDL-key-scan-codes.patch
 git am ../patch/0001-sandbox-make-SDL-window-resizable.patch
 git am ../patch/0001-sandbox-avoid-duplicate-backslash-input.patch
