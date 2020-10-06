2678d0bcc8452908c1f8f477fd87cd77272f9875
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-10-07 00:03:53 +0200

    0001-efi_loader-separate-device-tree-handling.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -91,6 +91,8 @@ git am ../patch/0001-examples-do-not-build-standalone-applications-with-F.patch
 # efi-2021-01
 # git am ../patch/0001-cmd-add-tool-to-correct-UEFI-binaries.patch
 
+git am ../patch/0001-efi_loader-separate-device-tree-handling.patch
+
 exit
 
 git am ../patch/0001-dfu-rename-dfu_tftp_write-to-dfu_write_by_name.patch
