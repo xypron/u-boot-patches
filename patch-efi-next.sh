f2103881ee5883f21166d5252035b19d436f991b
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-11-30 10:00:58 +0100

    0001-doc-allow-building-htmldoc-with-Sphinx-3.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -27,6 +27,7 @@ git am ../patch/0001-net-sntp-remove-CONFIG_TIMESTAMP-constraint.patch
 git am ../patch/0001-video-clean-up-TrueType-in-Kconfig.patch
 git am ../patch/0001-cmd-CMD_CPU-depends-on-CPU.patch
 git am ../patch/0001-test-test-lib-test_print.c-depends-on-CONSOLE_RECORD.patch
+git am ../patch/0001-doc-allow-building-htmldoc-with-Sphinx-3.patch
 
 # FAT file system
 git am ../patch/0001-common-always-compile-fixup_cmdtable.patch
