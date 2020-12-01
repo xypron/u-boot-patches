0186a2bee5fe9a819b45d3a30f731b946047fe4c
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-12-01 17:33:36 +0100

    0001-riscv-qemu-enable-distro-boot-from-scsi.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -28,6 +28,7 @@ git am ../patch/0001-video-clean-up-TrueType-in-Kconfig.patch
 git am ../patch/0001-cmd-CMD_CPU-depends-on-CPU.patch
 git am ../patch/0001-test-test-lib-test_print.c-depends-on-CONSOLE_RECORD.patch
 git am ../patch/0001-doc-allow-building-htmldoc-with-Sphinx-3.patch
+git am ../patch/0001-riscv-qemu-enable-distro-boot-from-scsi.patch
 
 # FAT file system
 git am ../patch/0001-common-always-compile-fixup_cmdtable.patch
