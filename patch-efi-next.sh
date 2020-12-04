608483bd69846b4cc562699c2e9f32c4fb4ba7ca
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-12-04 03:39:10 +0100

    0001-efi_loader-move-EFI_LOAD_FILE2_PROTOCOL_GUID.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -126,6 +126,7 @@ git am ../patch/0001-test-py-efi_capsule-test-for-raw-image-capsule.patch
 git am ../patch/0001-sandbox-enable-capsule-update-for-testing.patch
 
 git am ../patch/0001-efi_loader-resequence-functions-in-efi_boottime.c.patch
+git am ../patch/0001-efi_loader-move-EFI_LOAD_FILE2_PROTOCOL_GUID.patch
 
 exit
 
