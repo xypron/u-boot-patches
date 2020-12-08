d1ff96ac0b4ff84bedcbf3364dbea56982d46266
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-12-08 18:37:54 +0100

    0001-efi_selftest-test-EFI_LOAD_FILE_PROTOCOL.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -131,6 +131,7 @@ git am ../patch/0001-efi_loader-pass-boot_policy-to-efi_load_image_from_p.patch
 git am ../patch/0001-efi_loader-carve-out-efi_load_image_from_file.patch
 git am ../patch/0001-efi_loader-support-EFI_LOAD_FILE_PROTOCOL.patch
 git am ../patch/0001-efi_selftest-clean-up-Makefile.patch
+git am ../patch/0001-efi_selftest-test-EFI_LOAD_FILE_PROTOCOL.patch
 
 git am ../patch/0001-efi_loader-remove-EFI_HII_CONFIG_ROUTING_PROTOCOL.patch
 
