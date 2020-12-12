9f6e798432a487164727594645d5641fc41cf3e7
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-12-12 08:39:52 +0100

    0001-doc-move-README.commands-to-HTML-doc.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -23,6 +23,7 @@ git am ../patch/0001-cmd-CMD_CPU-depends-on-CPU.patch
 git am ../patch/0001-test-test-lib-test_print.c-depends-on-CONSOLE_RECORD.patch
 git am ../patch/0001-doc-allow-building-htmldoc-with-Sphinx-3.patch
 git am ../patch/0001-riscv-qemu-enable-distro-boot-from-scsi.patch
+git am ../patch/0001-doc-move-README.commands-to-HTML-doc.patch
 
 # FAT file system
 git am ../patch/0001-common-always-compile-fixup_cmdtable.patch
