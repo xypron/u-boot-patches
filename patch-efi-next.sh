1ad55c6d7cdea462d7c8dea1d94d05193e9e66e4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2020-12-25 13:46:10 +0100

    0001-fs-btrfs-simplify-close_ctree_fs_info.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -19,6 +19,7 @@ git am ../patch/0001-net-sntp-remove-CONFIG_TIMESTAMP-constraint.patch
 git am ../patch/0001-video-clean-up-TrueType-in-Kconfig.patch
 git am ../patch/0001-cmd-CMD_CPU-depends-on-CPU.patch
 git am ../patch/0001-test-test-lib-test_print.c-depends-on-CONSOLE_RECORD.patch
+git am ../patch/0001-fs-btrfs-simplify-close_ctree_fs_info.patch
 
 # FAT file system
 git am ../patch/0001-common-always-compile-fixup_cmdtable.patch
