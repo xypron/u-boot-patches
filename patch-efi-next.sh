41da186e847314644b9aa4e994c24ec88e0b4943
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-02-11 07:28:20 +0100

    0001-buildman-Thread-object-has-no-attribute-isAlive.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -18,6 +18,7 @@ git am ../patch/0001-cmd-riscv-sbi-support-System-Reset-Extension.patch
 git am ../patch/0001-cmd-CONFIG_CMD_MMC-depends-on-CONFIG_MMC.patch
 git am ../patch/0001-Add-support-for-stack-protector.patch
 git am ../patch/0001-malloc-adjust-memcpy-and-memset-definitions.patch
+git am ../patch/0001-buildman-Thread-object-has-no-attribute-isAlive.patch
 
 # Documentation
 
