8b98ab4d904471695a50898640422df976b18ed9
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-02-28 09:56:03 +0100

    0001-video-add-DejaVu-Mono-font.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -28,6 +28,7 @@ git am ../patch/0001-dm-ddr-socfpga-don-t-assign-values-that-are-not-used.patch
 git am ../patch/0001-mtd-rawnand-cortina_nand-missing-initialization.patch
 git am ../patch/0001-net-cortina_ni-buffer-overrun.patch
 git am ../patch/0001-disk-part-partitions-are-not-legacy.patch
+git am ../patch/0001-video-add-DejaVu-Mono-font.patch
 
 # Odroid HC4
 git am ../patch/0001-ARM-dts-add-Odroid-HC4-device-tree.patch
