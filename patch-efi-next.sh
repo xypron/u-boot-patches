0d20ff4abb3cc9784ca2fcb78c729aff389e3785
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-03-27 12:59:25 +0100

    0001-efi_loader-Cleanup-get_var-duplication.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -79,6 +79,8 @@ git am ../patch/0001-trace-avoid-infinite-recursion.patch
 
 # UEFI
 
+git am ../patch/0001-efi_loader-Cleanup-get_var-duplication.patch
+
 # git am ../patch/0001-fs-fat-fix-file_fat_detectfs.patch
 # git am ../patch/0001-efi_loader-size-check-if-EFI_DT_INSTALL_TABLE.patch
 
