177e2dc3d4bc2d64da6f57125096b03e94b4b9a7
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-03-28 10:58:07 +0200

    0001-linker_lists-document-ll_entry_ref-parameters.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -19,6 +19,7 @@ git am ../patch/0001-imx6-icorem6-chmod-644-enigcam.bmp.patch
 git am ../patch/0001-riscv-assembler-versions-of-memcpy-memmove-memset.patch
 git am ../patch/0001-riscv-simplify-longjmp.patch
 git am ../patch/0001-test-unit-test-for-longjmp.patch
+git am ../patch/0001-linker_lists-document-ll_entry_ref-parameters.patch
 
 # Odroid HC4
 git am ../patch/0001-ARM-dts-add-Odroid-HC4-device-tree.patch
