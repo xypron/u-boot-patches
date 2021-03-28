51e4b89cbbfd4dd459d0e00a67ad9001e7293b07
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-03-28 12:01:02 +0200

    0001-efi_loader-documentation-codepage_437.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -85,6 +85,7 @@ git am ../patch/0001-efi_loader-Cleanup-get_var-duplication.patch
 git am ../patch/0001-fs-fat-fix-file_fat_detectfs.patch
 git am ../patch/0001-doc-Update-uefi-documentation-for-initrd-loading-opt.patch
 git am ../patch/0001-efi_loader-improve-documentation-of-enum-efi_test_ph.patch
+git am ../patch/0001-efi_loader-documentation-codepage_437.patch
 
 # git am ../patch/0001-efi_loader-size-check-if-EFI_DT_INSTALL_TABLE.patch
 
