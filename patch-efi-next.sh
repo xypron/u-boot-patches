e1fa0a4952d728c58f0c7205bc644b08c569e2e4
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-04-11 20:30:32 +0200

    0001-doc-fatinfo-man-page.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -81,6 +81,7 @@ git am ../patch/0001-trace-avoid-infinite-recursion.patch
 # doc:
 git am ../patch/0001-board-toradex-apalis-imx8x-fix-build-warning.patch
 git am ../patch/0001-doc-imx-psb-fix-footnotes.patch
+git am ../patch/0001-doc-fatinfo-man-page.patch
 # EFI:
 git am ../patch/0001-efi_loader-esrt-Remove-EFI_CALL-invocation-for-efi_c.patch
 git am ../patch/0001-efi_loader-memory-leak-in-efi_capsule_scan_dir.patch
