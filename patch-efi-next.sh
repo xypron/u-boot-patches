b5757f9b223f8933285daf38c3268e7284e52fec
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-05-15 20:15:28 +0200

    0001-sandbox-fix-sandbox_reset.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -59,7 +59,8 @@ git am ../patch/0001-mtd-cfi_flash-read-device-tree-correctly.patch
 # git am ../patch/0001-configs-sandbox-enable-CONFIG_CMD_BOOTEFI_SELFTEST.patch
 git am ../patch/0001-sandbox-add-missing-definitions-for-flash-drivers.patch
 git am ../patch/0001-sandbox-ensure-that-state-ram_buf-is-in-low-memory.patch
-gti am ../patch/0001-sandbox-correct-determination-of-the-text-base.patch
+git am ../patch/0001-sandbox-correct-determination-of-the-text-base.patch
+git am ../patch/0001-sandbox-fix-sandbox_reset.patch
 
 git am ../patch/0001-video-add-DejaVu-Mono-font.patch
 git am ../patch/0001-video-support-colors-in-truetype-console.patch
