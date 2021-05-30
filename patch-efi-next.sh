5216558fac531a7e01e4ea1bdffd303a2e1ea529
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-05-30 22:23:36 +0200

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -80,9 +80,6 @@ git am ../patch/0001-trace-avoid-infinite-recursion.patch
 # doc:
 
 # EFI:
-git am ../patch/0001-Revert-lib-introduce-HASH_CALCULATE-option.patch
-git am ../patch/0001-efi_loader-Work-around-build-issue-due-to-missing-ha.patch
-git am ../patch/0001-efi_loader-add-PE-COFF-image-measurement.patch
 
 # GRUB
 git am ../patch/0001-efi_loader-add-Linux-magic-to-RISC-V-crt0.patch
