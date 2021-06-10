be29ca5d94ec3903a77f42d006cb6b0bbf638f6b
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-06-10 12:28:51 +0200

    0001-smbios-convert-function-descriptions-to-Sphinx-style.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -15,6 +15,7 @@ git am ../patch/0001-configs-qemu-riscv64_spl_defconfig-enable-CMD_SBI.patch
 git am ../patch/0001-smbios-error-handling-for-invalid-addresses.patch
 git am ../patch/0001-net-sun8i-emac-fix-MDIO-frequency.patch
 git am ../patch/0001-test-NULL-dereference-in-test-uclass.patch
+git am ../patch/0001-smbios-convert-function-descriptions-to-Sphinx-style.patch
 
 # Odroid HC4
 git am ../patch/0001-ARM-dts-add-Odroid-HC4-device-tree.patch
