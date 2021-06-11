2dbb03eaac63d04d3eecbdeef22410a60cb9a932
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-06-12 00:06:59 +0200

    0001-efidebug-correct-display-of-BootOrder.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -17,7 +17,6 @@ git am ../patch/0001-net-sun8i-emac-fix-MDIO-frequency.patch
 git am ../patch/0001-test-NULL-dereference-in-test-uclass.patch
 git am ../patch/0001-smbios-convert-function-descriptions-to-Sphinx-style.patch
 git am ../patch/0001-arm64-mvebu-correct-Armada-8K-addresses.patch
-git am ../patch/0001-efidebug-correct-display-of-BootOrder.patch
 
 # Odroid HC4
 git am ../patch/0001-ARM-dts-add-Odroid-HC4-device-tree.patch
@@ -85,6 +84,7 @@ git am ../patch/0001-trace-avoid-infinite-recursion.patch
 
 # EFI:
 git am ../patch/0001-efi_loader-TPL_HIGH_LEVEL-not-allowed-for-CreateEven.patch
+git am ../patch/0001-efidebug-correct-display-of-BootOrder.patch
 
 # GRUB
 git am ../patch/0001-efi_loader-add-Linux-magic-to-RISC-V-crt0.patch
