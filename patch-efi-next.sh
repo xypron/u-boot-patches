3ae87418de47822fe46ba74dc97b7b84dcead210
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-06-21 07:59:42 +0200

    0001-smbios-Fix-SMBIOS-tables.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -88,6 +88,7 @@ git am ../patch/0001-doc-usage-cmd-usage-help-file-for-askenv.patch
 git am ../patch/0001-efi_loader-TPL_HIGH_LEVEL-not-allowed-for-CreateEven.patch
 git am ../patch/0001-efidebug-correct-display-of-BootOrder.patch
 git am ../patch/0001-smbios-Fix-BIOS-Characteristics-Extension-Byte-2.patch
+git am ../patch/0001-smbios-Fix-SMBIOS-tables.patch
 
 # efi-next
 git am ../patch/0001-efi_loader-improve-block-device-integration-with-DM.patch
