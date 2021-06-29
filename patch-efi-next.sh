2f7a2087141f4717198ea57ed1e3a248d08c9b48
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-06-29 10:12:53 +0200

    0001-efi_loader-missing-parentheses-in-query_console_size.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -88,8 +88,9 @@ git am ../patch/0001-smbios-Fix-BIOS-Characteristics-Extension-Byte-2.patch
 git am ../patch/0001-smbios-Fix-SMBIOS-tables.patch
 
 # efi-next
-git am ../patch/0001-efi_loader-improve-block-device-integration-with-DM.patch
 git am ../patch/0001-efi_loader-fix-set_capsule_result.patch
+git am ../patch/0001-efi_loader-missing-parentheses-in-query_console_size.patch
+git am ../patch/0001-efi_loader-improve-block-device-integration-with-DM.patch
 
 # GRUB
 git am ../patch/0001-efi_loader-add-Linux-magic-to-RISC-V-crt0.patch
