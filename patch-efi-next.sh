a1fcb5aa932e9b6e8175f6c159722994fb2b38d0
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-07-18 09:45:56 +0200

    0001-efi_loader-set-partition-GUID-in-device-path-for-SIG.patch
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch-efi-next.sh
+++ patch-efi-next.sh
@@ -83,6 +83,7 @@ git am ../patch/0001-trace-avoid-infinite-recursion.patch
 git am ../patch/0001-efi_loader-set-partition-GUID-in-device-path-for-SIG.patch
 git am ../patch/0001-efi_loader-Use-pD-to-log-device-path-instead-of-loca.patch
 git am ../patch/0001-efi_loader-increase-eventlog-buffer-size.patch
+git am ../patch/0001-efi_loader-set-partition-GUID-in-device-path-for-SIG.patch
 
 exit
 # DM integration
